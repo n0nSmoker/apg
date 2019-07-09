7a6c688b9e65db7cdf632cb138db00f5c485a0ad
miko <stoprobots@gmail.com>
2019-07-09 20:46:38 +0300

    Fixed package info

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup
 
 setup(
     name='apg',
-    version='0.3.2',
+    version='0.3.4',
     description='Awesome project generation tool',
     author='Y-Bro',
     url='https://github.com/n0nSmoker/apg',
     keywords=['apg', 'generate project', 'framework', 'cookie-cutter'],
-    packages=['apg.apg'],
-    py_modules=['apg'],
+    packages=['apg', 'apg.utils'],
+    py_modules=['apg', 'apg.utils'],
     install_requires=[
         'Click',
         'PyYAML',
