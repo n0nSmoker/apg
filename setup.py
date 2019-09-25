c541d82476bffd22b01560b75cc7e7d534a16d43
miko <stoprobots@gmail.com>
2019-09-25 17:44:15 +0300

    Added template files to the package

--- setup.py
+++ setup.py
@@ -3,13 +3,14 @@ from setuptools import setup
 
 setup(
     name='apg',
-    version='0.3.4',
+    version='0.4.3',
     description='Awesome project generation tool',
     author='Y-Bro',
     url='https://github.com/n0nSmoker/apg',
     keywords=['apg', 'generate project', 'framework', 'cookie-cutter'],
     packages=['apg', 'apg.utils'],
     py_modules=['apg', 'apg.utils'],
+    include_package_data=True,
     install_requires=[
         'Click',
         'PyYAML',
