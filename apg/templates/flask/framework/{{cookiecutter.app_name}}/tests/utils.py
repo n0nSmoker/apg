5b694d9dfce58417765e5e45e0c88c9a27fd2264
Y <426458+n0nSmoker@users.noreply.github.com>
2019-11-02 13:47:55 +0300

    Update utils.py

--- apg/templates/flask/framework/{{cookiecutter.app_name}}/tests/utils.py
+++ apg/templates/flask/framework/{{cookiecutter.app_name}}/tests/utils.py
@@ -29,31 +29,29 @@ class Client:
     def __init__(self, app):
         self.app = app
 
-    @staticmethod
-    def _get_url(endpoint, **values):
-        return url_for(endpoint=endpoint, **values)
-
-    @staticmethod
-    def _get_data(resp, check_status=None):
-        if check_status:
-            assert resp.status_code == check_status, resp.data
-        data = resp.data
-        if resp.content_type == 'application/json':
-            data = json.loads(resp.data)
-        return data
-
-    def send(self, endpoint, method, data=None, check_status=200, content_type=None, headers=None, **values):
+    def send(self, endpoint, method, data=None, check_status=200, \
+             content_type=None, headers=None, **values):
+        
         kwargs = {}
-        url = self._get_url(endpoint=endpoint, **values)
+        url = url_for(endpoint=endpoint, **values)
         func = getattr(self.app.client, method)
+        
         if data:
             kwargs['data'] = data
         if content_type:
             kwargs['content_type'] = content_type
         if headers:
             kwargs['headers'] = headers
+            
         resp = func(url, **kwargs)
-        return self._get_data(resp, check_status=check_status)
+        
+        if check_status:
+            assert resp.status_code == check_status, resp.data
+            
+        data = resp.data
+        if resp.content_type == self.json_header:
+            data = json.loads(resp.data)
+        return data
 
     def get(self, **kwargs):
         return self.send(method='get', **kwargs)
@@ -62,14 +60,12 @@ class Client:
         return self.send(method='delete', **kwargs)
 
     def post(self, content_type=None, data=None, **kwargs):
-        content_type = content_type or self.json_header
-        if content_type == self.json_header:
+        if not content_type or content_type == self.json_header:
             data = json.dumps(data)
         return self.send(method='post', data=data, content_type=content_type, **kwargs)
 
     def put(self, content_type=None, data=None, **kwargs):
-        content_type = content_type or self.json_header
-        if content_type == self.json_header:
+        if not content_type or content_type == self.json_header:
             data = json.dumps(data)
         return self.send(method='put', data=data, content_type=content_type, **kwargs)
 
