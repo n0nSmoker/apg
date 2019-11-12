5fb21fa0dc340787278f2460003fb44ce2a4b526
Y <426458+n0nSmoker@users.noreply.github.com>
2019-11-12 17:23:43 +0300

    Update schemas.py

--- apg/templates/flask/framework/{{cookiecutter.app_name}}/app/common/schemas.py
+++ apg/templates/flask/framework/{{cookiecutter.app_name}}/app/common/schemas.py
@@ -2,7 +2,7 @@ from webargs import fields, validate
 
 
 FILTER_SCHEMA = {
-    'page': fields.Int(missing=1),
-    'limit': fields.Int(missing=10),
+    'page': fields.Int(missing=1, validate=validate.Range(min=1, max=100)),
+    'limit': fields.Int(missing=10, validate=validate.Range(min=1, max=1000)),
     'sort_by': fields.Str(validate=validate.OneOf(['id', 'created_at', 'updated_at']), missing='created_at')
 }
